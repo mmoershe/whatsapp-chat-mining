ca1227c9b4636e9d13b54bdf3d5b4a6dfa52f6fd
mmoershe <92526921+mmoershe@users.noreply.github.com>
2024-02-09 03:41:09 +0100

    filtering data and creating clean.txt

--- modules/txt_operations.py
+++ modules/txt_operations.py
@@ -42,25 +42,44 @@ def create_clean_temp_dir(TEMP_DIR: str) -> None:
 
 
 def clean_txt(input_dir: str, clean_dir: str) -> None: 
-    shutil.copy(input_dir, os.path.join(os.path.dirname(input_dir), clean_dir))
+    ERRORS: list = [
+        "Nachrichten und Anrufe sind Ende-zu-Ende-verschlüsselt. Niemand außerhalb dieses Chats kann sie lesen oder anhören, nicht einmal WhatsApp. Tippe, um mehr zu erfahren.",
+		"hat sich geändert. Tippe, um mehr zu erfahren."
+    ]
+    
+    # retrieve erorrs that are  encased by <>   
     with open(input_dir, 'r', encoding='utf-8') as f:
-        errors: list = set()
+        special_errors: list = set()
         for i, line in enumerate(f.readlines()):
             line: str = line.strip()
             if '<' in line and '>' in line: 
-                errors.add(line[line.find('<'):line.find('>')+1:])
-    print(errors)
+                special_errors.add(line[line.find('<'):line.find('>')+1:])
+    for special_error in list(special_errors):
+        ERRORS.append(special_error)
+                
+    with open(input_dir, 'r', encoding='utf-8') as input_txt, open(clean_dir, 'w', encoding='utf-8') as destination_txt:
+        for line in input_txt: 
+            if any(error in line for error in ERRORS):
+                continue
+            destination_txt.write(line)
+        
+
+
 
 
 def create_simple_metadata(origin: str, destination: str) -> None:
     # ^(\d{2})\.(\d{2})\.(\d{2})\,\s(\d{2})\:(\d{2})\s\-
     metadata: dict = dict()
     with open(origin, 'r', encoding = 'utf-8') as origin: 
+        line_amount: int = 0
         names = set()
         for line in origin.readlines():
+            line_amount += 1
             if not search(r"^(\d{2})\.(\d{2})\.(\d{2})\,\s(\d{2})\:(\d{2})\s\-", line):
                 continue
             line: str = line[line.find('-')+1::]
             line: str = line[:line.find(':'):].strip()
             names.add(line)
+        names = list(names)
     print(f"{names = }")
+    print(f"{line_amount = }")
